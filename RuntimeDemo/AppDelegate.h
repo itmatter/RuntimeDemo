88faa87f98cce1268c1465766eea643f8822ff0f
Li_guang <liliguang@LLGMBP.local>
2017-08-16 18:43:13 +0800

    Initial Commit

---
+++ RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by 李礼光 on 2017/8/16.
+//  Copyright © 2017年 LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RuntimeDemo
+//
+//  Created by 李礼光 on 2017/8/16.
+//  Copyright © 2017年 LG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
